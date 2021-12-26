5fbafdeb7e23d42ac27cbef80d12678b4bc4535e
Melih <85121442+YavasLan@users.noreply.github.com>
2021-12-26 21:29:05 +0300

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("naber paşam");
+
+Console.WriteLine("Ad");
+string name = Console.ReadLine();
+Console.WriteLine("Soyad");
+string surname= Console.ReadLine();
+Console.WriteLine("Merhaba"+" "+ name + " " + surname);
